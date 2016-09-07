f081abdd649f18c96feabcb54ede74dde5e54ead
leopiccionia <leopiccionia@gmail.com>
2016-09-07 18:34:29 -0300

    TAR.GZ packing.

--- signet.py
+++ signet.py
@@ -3,10 +3,22 @@
 #requires: bcrypt, pycrypt
 
 import getpass, os, sys
-import bcrypt, base64
+import tarfile
+import bcrypt
 from Crypto.Cipher import AES
 
 # Utils
+
+def get_factor():
+	factor = int(input('Work factor (min = 4; max = 31; default = 18): '))
+	return factor if factor and factor > 3 and factor < 32 else 18
+
+def get_files(dir):
+	files_list = []
+	for root, subdirs, filenames in os.walk(dir):
+		for filename in filenames:
+			files_list.append(os.path.join(root, filename))
+	return files_list
 
 def get_masterkey(password, factor):
 	binary_password = password.encode()
@@ -15,10 +27,6 @@ def get_masterkey(password, factor):
 	master_key = bcrypt.kdf(password = binary_password, salt = b'sealed', desired_key_bytes = 32, rounds = factor)
 	print('Master key generated.')
 	return master_key
-
-def get_factor():
-	factor = int(input('Work factor (min = 4; max = 31; default = 18): '))
-	return factor if factor and factor > 3 and factor < 32 else 18
 
 def get_password():
 	password = getpass.getpass('Password: ')
@@ -32,28 +40,30 @@ def get_password():
 
 def pack(input_path, output_path):
 	if not os.path.isdir(input_path):
-		sys.stderr.write('Invalid input path: ' + input_path + '.\n')
+		sys.stderr.write('Invalid directory: ' + input_path + '.\n')
 	elif not os.path.isdir(output_path):
-		sys.stderr.write('Invalid output path: ' + output_path + '.\n')
+		sys.stderr.write('Invalid directory: ' + output_path + '.\n')
 	else:
 		master_key = get_masterkey(get_password(), get_factor())
-		#print base64.b64encode(master_key)
+		with tarfile.open('signet.tar.gz', 'w:gz') as tar:
+			for name in get_files(input_path):
+				tar.add(name)
 		pass #TODO
 
 # Unpacking
 
 def unpack(input_path, output_path):
 	if not os.path.isdir(input_path):
-		sys.stderr.write('Invalid input path: ' + input_path + '.\n')
+		sys.stderr.write('Invalid directory: ' + input_path + '.\n')
 	elif not os.path.isdir(output_path):
-		sys.stderr.write('Invalid output path: ' + output_path + '.\n')
+		sys.stderr.write('Invalid directory: ' + output_path + '.\n')
 	else:
 		pass #TODO
 
 # Treating input
 
-input_path = sys.argv[2] if len(sys.argv) > 2 else '.'
-output_path = sys.argv[3] if len(sys.argv) > 3 else '.'
+input_path = sys.argv[2] if len(sys.argv) > 2 else os.path.expanduser('~')
+output_path = sys.argv[3] if len(sys.argv) > 3 else os.path.expanduser('~')
 
 if len(sys.argv) > 1:
 	if sys.argv[1] == '-p' or sys.argv[1] == '--pack':
